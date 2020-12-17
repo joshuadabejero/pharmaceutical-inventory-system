90e170d992660b13fc9f66fbfebf406bfe64e5fd
Joshua Dawi Abejero <joshuadabejero@gmail.com>
2020-12-17 19:14:57 +0800

    feat: add User model

---
+++ src/models/User.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author joshuadabejero
+ */
+public class User {
+    String fname;
+    String mname;
+    String lname;
+    String gender;
+    int month;
+    int day;
+    int year;
+    String address;
+    String cnumber;
+}
