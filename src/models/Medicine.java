5b39ae5895856f22f30c9d95b7eee741ada311ec
John Troi Dawi Abejero <johntroiprogatelight@gmail.com>
2020-12-17 19:34:27 +0800

    Feat: add Medicine model

---
+++ src/models/Medicine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author johntroidabejero
+ */
+public class Medicine {
+    int medicineid;
+    String brand;
+    int day;
+    int month;
+    int year;
+    String description;
+}
