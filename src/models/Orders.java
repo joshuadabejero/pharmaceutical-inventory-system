8a95887468d71289268286bb313802f0ad4b5a11
John Troi Dawi Abejero <johntroiprogatelight@gmail.com>
2020-12-17 22:40:00 +0800

    Feat: add Medications models

--- src/models/Orders.java
+++ src/models/Orders.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package models;
 
 import java.util.Date;
 
@@ -11,16 +11,12 @@ import java.util.Date;
  *
  * @author johntroidabejero
  */
-public class Medicine {
+public class Orders {
+    int orderid;
+    Date receipt;
     int medicineid;
-    String name;
-    String brand;
-    int price;
-    int quantity;
-    int weight;
-    String type;
-    boolean steriod;
-    String description;
-    Date expiration;
-    int medicationid;
+    int quatinty;
+    int cost;
+    int customeid;
+    int employee;
 }
